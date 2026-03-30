9c61d4d4982dee35fa32e03f553ed1a6cbc4c54b
aaronv <41397746+aaron70@users.noreply.github.com>
2026-03-30 00:08:44 -0600

    Add gpd host

---
+++ modules/hosts/gpd.nix
@@ -0,0 +1,67 @@
+{ inputs, self, ... }: 
+
+let
+  host = "gpd";
+in {
+  flake.nixosConfigurations.${host} = inputs.nixpkgs.lib.nixosSystem {
+    modules = [ self.nixosModules.${host} ];
+  };
+
+  flake.nixosModules.${host} = { ... }: {
+    config = {
+      information = {
+        isLaptop = true;
+        hasBluetooth = true;
+        hasBattery = true;
+      };
+
+      preferences = {
+        profile = "personal";
+
+        desktop.enable = true;
+      };
+    };
+  };
+
+  flake.nixosModules.${host} = { config, lib, pkgs, modulesPath, ... }: {
+    imports =
+      [ (modulesPath + "/installer/scan/not-detected.nix")
+      ];
+  
+    boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "usbhid" "sd_mod" "sdhci_pci" ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ "kvm-amd" ];
+    boot.extraModulePackages = [ ];
+  
+    fileSystems."/" =
+      { device = "/dev/disk/by-uuid/a382f749-eb68-4cd7-b3ac-4e96d34eb719";
+        fsType = "ext4";
+      };
+  
+    fileSystems."/boot" =
+      { device = "/dev/disk/by-uuid/E84A-8A5C";
+        fsType = "vfat";
+        options = [ "fmask=0077" "dmask=0077" ];
+      };
+  
+  
+    fileSystems."/home/aaronv/shared-home" =
+      { device = "/dev/disk/by-uuid/6AB20C7DB20C504D";
+        fsType = "ntfs";
+        options = [ "users" "nofail" "exec" "rw" "uid=1000" "gid=100" ];
+      };
+  
+  
+    swapDevices = [ ];
+  
+    # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+    # (the default) this is the recommended approach. When using systemd-networkd it's
+    # still possible to use this option, but it's recommended to use it in conjunction
+    # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+    networking.useDHCP = lib.mkDefault true;
+    # networking.interfaces.wlp195s0.useDHCP = lib.mkDefault true;
+  
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  };
+}
