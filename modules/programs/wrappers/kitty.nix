e27c4dbc84f6a035b6a40f53aa33e59298aa31b0
aaronv <aarongeovanny70@gmail.com>
2026-04-01 02:46:51 -0600

    Fix fonts for kitty

--- modules/programs/wrappers/kitty.nix
+++ modules/programs/wrappers/kitty.nix
@@ -13,6 +13,10 @@ with lib;
         include ${config.configurations.theme.path}
         confirm_os_window_close 0 
         enable_audio_bell false
+        font_family      JetBrainsMono Nerd Font
+        bold_font        auto
+        italic_font      auto
+        bold_italic_font auto
 
         shell ${getExe config.configurations.shell}
       '';
