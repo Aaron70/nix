5a069f214cb319622614a2c8aaa3aa70149a03c9
aaronv <41397746+aaron70@users.noreply.github.com>
2026-03-31 20:33:18 -0600

    Update laptop host hardawre configurations

--- modules/hosts/laptop.nix
+++ modules/hosts/laptop.nix
@@ -29,9 +29,7 @@ in {
   };
 
   flake.nixosModules."${host}-hardware" = { config, lib, pkgs, modulesPath, ... }: {
-    imports =
-      [ (modulesPath + "/installer/scan/not-detected.nix")
-      ];
+    imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
   
     boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
     boot.initrd.kernelModules = [ ];
@@ -39,7 +37,7 @@ in {
     boot.extraModulePackages = [ ];
   
     fileSystems."/" =
-      { device = "/dev/disk/by-uuid/d7411d9a-9f9a-4d9f-9579-6b16800ad339";
+      { device = "/dev/disk/by-uuid/f60eed8e-8feb-4c44-8c77-7cfcf9aa41ba";
         fsType = "ext4";
       };
   
@@ -50,14 +48,6 @@ in {
       };
   
     swapDevices = [ ];
-  
-    # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-    # (the default) this is the recommended approach. When using systemd-networkd it's
-    # still possible to use this option, but it's recommended to use it in conjunction
-    # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-    networking.useDHCP = lib.mkDefault true;
-    # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
-    # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
   
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
     hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
