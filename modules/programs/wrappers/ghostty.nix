2312e25090e47eda95837789adc3841093559b47
aaronv <aarongeovanny70@gmail.com>
2026-04-01 23:02:23 -0600

    migrating the terminal packages

--- modules/programs/wrappers/ghostty.nix
+++ modules/programs/wrappers/ghostty.nix
@@ -1,10 +1,27 @@
 { self, lib, ... }: 
 
 with lib;
-{
-  flake.wrappers.ghostty = { config, ... }: {
+let
+  name = "ghostty";
+in {
+
+  flake.nixosModules.programs = self.lib.mkNixosProgram name ({ ... }: {});
+
+  flake.programs.${name} = self.lib.mkProgram name ({ cfg, ... }@inputs: let
+    definition = self.definitions.programs.terminal inputs;
+  in {
+    options = definition.options;
+    config = {
+      package = self.wrappers.${name}.wrap {
+        inherit pkgs;
+        configurations = cfg.configurations;
+      };
+    };
+  });
+
+  flake.wrappers.${name} = { config, ... }: {
     imports = [ 
-      self.programs.terminal
+      self.definitions.programs.terminal
       self.wrapperModules._ghostty 
     ];
 
